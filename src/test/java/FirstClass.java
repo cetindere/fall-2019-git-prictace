6c0e242a3232c973c24c062b47f9b3fbd6703c58
cetindere <cetinderedc@gmail.com>
2020-03-18 20:02:29 -0400

    updated

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,6 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello Hello");
-        System.out.println("Second update");
+        System.out.println("Second ");
+        System.out.println("Hi. How are you?");
     }
 }
